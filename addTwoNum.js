92161b13b39df60d9ae762f531bd3b9f5a722149
utshaweu <utshaw1995@gmail.com>
2023-09-29 17:47:41 +0600

    add two num and square root added in js

---
+++ addTwoNum.js
@@ -0,0 +1,15 @@
+const num1 = 5;
+const num2 = 6;
+
+const sum = num1 + num2;
+
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+//Add Two Numbers Entered by the user
+
+const num3 = parseInt(prompt("Enter the first number"));
+const num4 = parseInt(prompt("Enter the second number"));
+
+const sum2 = num3 + num4;
+
+console.log(`The sum of ${num3} and ${num4} is ${sum2}`)
